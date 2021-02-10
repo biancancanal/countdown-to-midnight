bf1906e27795710d5dfa61d001335ce112225d31
Bianca <biancancanal@gmail.com>
2021-02-09 21:43:42 -0600

    commit tree 938667d7c7554848350e7c11a0145018ee4dccf3

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+
+def countdown(n)
+    while n > 0
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    if n == 0
+       "HAPPY NEW YEAR!"
+    end
+end
+
+
+def countdown_with_sleep(n)
+    while n > 0
+        sleep 5
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    if n == 0
+       "HAPPY NEW YEAR!"
+    end
 end
